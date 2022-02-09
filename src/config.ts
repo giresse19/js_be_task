fc8f7b5b7ed7d840b0e271e6506f25a9c86fadf0
giresse19 <ashubobg@yahoo.co.uk>
2022-02-09 10:47:31 +0200

    Handle retries when request fails.

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
+import axiosRetry from 'axios-retry';
 
 const API_URL:string = 'https://api.veriff.internal';
 
-export const PORT:Number =  3000;
-export const PROBABILITY_EQUILIBRIUM:Number =  0.5;
+export const PORT:number =  3000;
+export const PROBABILITY_EQUILIBRIUM:number =  0.5;
 export const HOST:string = 'localhost';
 export const SESSION_UUID:string = '90d61876-b99a-443e-994c-ba882c8558b6';
 export const API = axios.create({
     baseURL: `${API_URL}`,
 });
+axiosRetry(API, { retries: 3 });
